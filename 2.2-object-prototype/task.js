e2ff20e14d282b07d15aed5b56ebd88c461b74d3
Nikita Efremov <nikita-efremov-00@mail.ru>
2021-03-10 12:39:41 +0400

    first commit object-prototype

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,44 @@
 //String.prototype.isPalindrome - для задачи №1
+String.prototype.isPalindrome = function() {
+    for (let i = 0, j = (this.length - 1); i < this.length, j > -1; i++, j--) {
+        if(this[i] == ' ') {
+            i++;
+        }
+
+        if(this[j] == ' ') {
+            j--;
+        }
+
+        if (this[i].toLowerCase() != this[j].toLowerCase()) {
+            return false;
+        }
+    }
+    return true;
+}
 
 function getAverageMark(marks) {
     // код для задачи №2 писать здесь
-    // return averageMark
+        if (marks.length == 0) {
+        return 0;
+    }
+
+    let average = 0;
+    for (let i = 0; i < marks.length; i++) {
+        average += marks[i];
+    }
+    average = average / marks.length;
+    let roundedAverage = Math.round(average);
+    return roundedAverage;
 }
 
 function checkBirthday(birthday) {
     // код для задачи №3 писать здесь
-    // return verdict
+    let now = +(new Date);
+    let year = birthday.slice(0, 4), month = birthday.slice(5, 7) - 1, day = birthday.slice(8, 10);
+    let birth = new Date(year, month, day);
+    birthday = +birth;
+
+    let diff = now - birthday;
+    let age = diff / 31536000000;
+    return age > 18;
 }
